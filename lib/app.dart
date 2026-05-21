99692f3f943ba896fa4fcef1e8f030ae22408eb9
Johan Persson <perssonjohan95@gmail.com>
2026-05-21 11:57:51 +0200

    Refactor app

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'pages/greenhouse_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Greenhouse',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 40, 166, 45)),
+      ),
+      home: const GreenhousePage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
-import 'pages/greenhouse_page.dart';
+import 'app.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Greenhouse',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 40, 166, 45)),
-      ),
-      home: const GreenhousePage(),
-    );
-  }
-}
 
 
